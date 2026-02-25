273507aae36dcf051a64c2044e0ab39937a64948
Philipe <philipefilho@hotmail.com>
2026-02-24 23:25:22 -0300

    fix: move `hello` route

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,7 +18,7 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/hello')
       .expect(200)
       .expect('Hello World!');
   });
